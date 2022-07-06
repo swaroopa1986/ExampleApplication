992a3f5cad786362184503832a380da555520774
swaroopa1986 <54567036+swaroopa1986@users.noreply.github.com>
2022-07-07 00:21:40 +0100

    updated circleci and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
-# Install pm2 which is a production process manager fro Node.js with a built-in load balancer.
+# Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
-#stop any instance of our application running currently
+# stop any instance of our application running currently
 pm2 stop example_app
-#change directory into folder where application is downloaded
+# change directory into folder where application is downloaded
 cd ExampleApplication/
-#Install application dependencies
+# Install application dependencies
 npm install
-echo $PRIVATE_KEY >privatekey.pem
+echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-#Start the application with the process name example_app using pm2
+# Start the application with the process name example_app using pm2
 pm2 start ./bin/www --name example_app
