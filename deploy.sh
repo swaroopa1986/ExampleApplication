ce98fc5db3418a8012f7160b5506905f012aa175
swaroopa1986 <54567036+swaroopa1986@users.noreply.github.com>
2022-07-07 00:08:03 +0100

    updated pipeline to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ pm2 stop example_app
 cd ExampleApplication/
 #Install application dependencies
 npm install
+echo $PRIVATE_KEY >privatekey.pem
+echo $SERVER > server.crt
 #Start the application with the process name example_app using pm2
 pm2 start ./bin/www --name example_app
