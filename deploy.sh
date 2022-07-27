2b2d84bea724c9ccf276fbe6ac020ef8b25aca91
swaroopa1986 <54567036+swaroopa1986@users.noreply.github.com>
2022-07-27 15:37:36 +0100

    added deployer

--- deploy.sh
+++ deploy.sh
@@ -11,21 +11,21 @@ fi
 # Pull down the instance from dockerhub
 docker pull $IMAGE_NAME
 
-# Check if a docker container exists with the name of node_app if it does remove the conatiner
-CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+# Check if a docker container exists with the name of CONTAINER_NAME if it does remove the conatiner
+CONTAINER_EXISTS=$(docker ps -a | grep CONTAINER_NAME)
 if [ "$CONTAINER_EXISTS" ]
 then
-    docker rm node_app
+    docker rm CONTAINER_NAME
 fi
-# Create a container called node_app that is available on port 8443 from our docker image
-docker create -p 8443:8443 --name node_app $IMAGE_NAME
+# Create a container called CONTAINER_NAME that is available on port 8443 from our docker image
+docker create -p 8443:8443 --name CONTAINER_NAME $IMAGE_NAME
 # Write the private key to a file
 echo $PRIVATE_KEY >privatekey.pem
 # Write the server key to file
 echo $SERVER > server.crt
-# Add the priavate key to the node_app docker container
-docker cp ./privatekey.pem node_app:/privatekey.pem
-# Add the server key to the node_app docker container
-docker cp ./server.crt node_app:/server.crt
-# start the node_app conatiner
-docker start node_app
+# Add the priavate key to the CONTAINER_NAME docker container
+docker cp ./privatekey.pem CONTAINER_NAME:/privatekey.pem
+# Add the server key to the CONTAINER_NAME docker container
+docker cp ./server.crt CONTAINER_NAME:/server.crt
+# start the CONTAINER_NAME conatiner
+docker start CONTAINER_NAME
